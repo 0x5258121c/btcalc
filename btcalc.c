ac0c6d6c2ce3e78973246b73d0d01829659e6fa0
btugu <btugux@gmail.com>
2021-07-15 22:10:23 +0300

    Initial commit

---
+++ btcalc.c
@@ -0,0 +1,620 @@
+/* 
+ * a simple calculator that uses a shunting yard algorithm,
+ * made by btugu
+ *
+ * */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include "btlib.h"
+
+#define MAXOP 64
+#define MAXNUM 64
+
+#ifndef BUFSIZE
+#define BUFSIZE MAXOP + MAXNUM
+#endif
+
+uint8_t validate_input(char*);
+double calculate(char*);
+void exec_params(void);
+
+// Infix expression interpretation
+
+enum {ASSOC_N = 0, ASSOC_L, ASSOC_R};
+
+typedef struct {
+
+	char op;
+	uint8_t prec, assoc;
+	_Bool unary;
+	double (*func)(double num1, double num2);
+
+} operator;
+
+operator ops[] = {
+
+	{'_', 4, ASSOC_R, 1, muminus},
+	{'^', 3, ASSOC_R, 0, mpow},
+	{'*', 2, ASSOC_L, 0, mmul},
+	{'/', 2, ASSOC_L, 0, mdiv},
+	{':', 2, ASSOC_L, 0, mdiv},
+	{'%', 2, ASSOC_L, 0, mmod},
+	{'+', 1, ASSOC_L, 0, madd},
+	{'-', 1, ASSOC_L, 0, msub},
+	{'(', 0, ASSOC_N, 0, NULL},
+	{')', 0, ASSOC_N, 0, NULL},
+
+};
+
+operator* opstack[MAXOP];
+uint8_t n_opstack = 0;
+
+double numstack[MAXNUM];
+uint8_t n_numstack = 0;
+
+operator* getop(uint8_t);
+void push_opstack(operator*);
+void insert_opstack(operator*);
+operator* pop_opstack(void);
+void push_numstack(double);
+void insert_numstack(double, char*);
+double pop_numstack(void);
+void process_op(operator*, char*);
+
+
+_Bool Param_found;
+_Bool Help_queued;
+_Bool Quit_queued;
+
+int main(int argc, char* argv[])
+{
+
+	_Bool valid;
+
+	if(argc == 1) {
+
+		system("clear");
+		printf("btugu's calculator\n\n");
+		printf("Enter 'h' or \"help\" for help.\n");
+		printf("Enter 'q' or \"quit\" to exit the program.\n\n");
+
+		while(1) {
+
+			printf("> ");
+			char* in = get_input(BUFSIZE);
+			in = rm_space(in);
+			valid = validate_input(in);
+
+			if(valid) {
+
+				exec_params();
+
+				if(Help_queued){
+
+					Help_queued = 0;
+					n_numstack = 0;
+					n_opstack = 0;
+					free(in);
+
+				} else {
+
+					printf("%f\n", calculate(in));
+					n_numstack = 0;
+					n_opstack = 0;
+					free(in);
+
+				}
+
+			}
+
+		}
+
+	}  
+
+	if(!argv[2]) {
+
+		switch(strasciivalsum(argv[1])) { // switch statement checks if argv[1] matches any of the valid parameters: '-h' and '--help'
+
+			case 149:
+			case 515:
+
+				Help_queued = 1;
+				exec_params();
+				exit(EXIT_SUCCESS);
+			
+		}
+
+	}
+
+	if(argc > 2) {
+
+		printf("Inline usage example:\n\n");
+		printf("btcalc \"32 * 2 - 5\"\n\n");
+		exit(EXIT_SUCCESS);
+
+	}
+
+	char* in = rm_space(argv[1]);
+
+	valid = validate_input(in);
+
+	if(valid) {
+
+		exec_params();
+
+		if(!Help_queued) {
+
+			printf("%f\n", calculate(in));
+			n_numstack = 0;
+			n_opstack = 0;
+			free(in);
+			exit(EXIT_SUCCESS);
+
+		}
+
+	}
+
+	exit(EXIT_FAILURE);
+
+}
+
+uint8_t validate_input(char* in)
+{
+
+	_Bool valid = 1;
+	uint8_t i;  
+
+	char* valid_nonalpha_chars = "%+-/:*().,";
+	char* temp = malloc(sizeof(char) * 4);
+
+	if(Help_queued)
+		exit(EXIT_SUCCESS);
+
+	for(i = 0; in[i] != '\0'; i++) {
+
+		if(isalpha(in[i])) {
+
+			for(uint8_t j = 0; j < 4; j++)
+				temp[j] = in[i];
+
+			if(!strcmp(temp, "help")) {
+
+				Help_queued = 1;
+				free(temp);
+				return valid;
+
+			} else if (!strcmp(temp, "quit")) {
+
+				Quit_queued = 1;
+				free(temp);
+				return valid;
+
+			}
+
+			if(in[i] == 'q' && i == 0) {
+
+				Quit_queued = 1;
+
+			} else if(in[0] == 'h' && i == 0) {
+
+				Help_queued = 1;
+
+			} else {
+
+				valid = 0;
+
+				fprintf(stderr, "ERROR: Invalid characters were used in input.\n");
+
+			}
+
+			return valid;
+
+		} else if(!(isdigit(in[i]) || ismemberof(valid_nonalpha_chars, in[i]))) {
+
+			valid = 0;
+
+			fprintf(stderr, "ERROR: Invalid characters were used in input.\n");
+
+			return valid;
+
+		}
+
+	}
+
+	return valid;
+
+}
+
+double calculate(char* in)
+{
+
+	double num1, num2;
+
+	operator* lastop = NULL;
+	operator* op = NULL;
+	operator* nextop = NULL;
+
+	for(uint8_t i = 0; in[i] != '\0'; i++) {
+
+		if(op = getop(in[i])) {
+
+			if(in[i + 1] == '\0') {
+				
+				if(op->op != ')') {
+
+					fprintf(stderr, "ERROR: Syntax error\n");
+					n_numstack = 0;
+					n_opstack = 0;
+					return EXIT_FAILURE;
+
+				}
+
+			} else if(nextop = getop(in[i + 1])) {
+
+				if((nextop->op != '(') && (nextop->op != ')')) {
+
+					if(op->op != ')') {
+
+						fprintf(stderr, "ERROR: Syntax error\n");
+						n_numstack = 0;
+						n_opstack = 0;
+						return EXIT_FAILURE;
+
+					}
+
+				}
+
+			}
+
+			if(op->op == '-' && !n_numstack)
+				op = getop('_');
+
+			if(!lastop) {
+
+				switch(op->op) {
+
+					case '(':
+
+						if(i == 0)
+							break;
+
+					case ')':
+
+						fprintf(stderr, "ERROR: Syntax error\n");
+						n_numstack = 0;
+						n_opstack = 0;
+						return EXIT_FAILURE;
+
+					case '%':
+					case '/':
+					case ':':
+					case '*':
+					case '^':
+
+						if(n_numstack == 0) {
+
+							fprintf(stderr, "ERROR: Syntax error\n");
+							n_numstack = 0;
+							n_opstack = 0;
+							return EXIT_FAILURE;
+
+						}
+
+						break;
+
+				}
+
+			}
+
+			process_op(op, in);
+			lastop = op;
+
+		} else {
+
+			uint8_t j = i;
+
+			while((getop(in[j]) == NULL) && (in[j] != '\0'))
+				j++;
+
+			push_numstack(strtodec(cut_str(in, i, j - 1)));
+			i = j - 1;
+
+		}
+
+	}
+
+	while(n_numstack > 1) {
+		
+		operator* popped_op = pop_opstack();
+		double num1 = pop_numstack();
+
+		if(popped_op->unary) {
+
+			push_numstack(popped_op->func(num1, 0));
+
+		} else {
+
+			double num2 = pop_numstack();
+			push_numstack(popped_op->func(num2, num1));
+
+		}
+
+	}
+
+	return numstack[0];
+
+}
+
+void exec_params(void)
+{
+
+	if(Help_queued) {
+
+		printf("\nValid inline arguments: \"-h\", \"--help\"\n\n");
+		printf("Valid app mode arguments: \"help\", \"h\", \"q\"\n\n");
+		printf("Valid characters are: Binary operators, digits, comma and dot\
+(both used to represent decimal numbers)\n\n");
+		printf("Example inline usage: btcalc \"32 * 2 - 5\"\n\n");
+
+	} else if(Quit_queued) {
+		
+		exit(EXIT_SUCCESS);
+
+	}
+
+}
+
+// Infix interpretation
+
+operator* getop(uint8_t c)
+{
+
+	for(uint8_t i = 0; i < (sizeof(ops) / sizeof(ops[0])); i++) {
+
+		if(ops[i].op == c)
+			return ops + i;
+
+	}
+
+	return NULL;
+
+}
+
+void push_opstack(operator* op)
+{
+
+	if(n_opstack > MAXOP - 1) {
+		
+		fprintf(stderr, "ERROR: Operator stack overflow\n");
+		exit(EXIT_FAILURE);
+
+	}
+
+	opstack[n_opstack++] = op;
+
+}
+
+void insert_opstack(operator* op)
+{
+
+	if(n_opstack > MAXOP - 1) {
+
+		fprintf(stderr, "ERROR: Operator stack overflow\n");
+		exit(EXIT_FAILURE);
+
+	}
+
+	uint8_t i = 0,j = 0;
+
+	for(i = 0; i < n_opstack; i++) {
+
+		if(opstack[i]->op == '(') {
+
+			j = i;
+			j++;
+
+		}
+
+	}
+
+	for(i = n_opstack++; i != j; i--)
+		opstack[i] = opstack[i - 1];
+
+	opstack[j] = op;
+
+}
+
+operator* pop_opstack()
+{
+
+	if(!n_opstack) {
+	
+		fprintf(stderr, "ERROR: Operator stack empty\n");
+		exit(EXIT_FAILURE);
+
+	}
+
+	--n_opstack;
+	return opstack[n_opstack];
+
+}
+
+
+void push_numstack(double num)
+{
+
+	if(n_numstack > MAXNUM - 1) {
+
+		fprintf(stderr, "ERROR: Number stack overflow\n");
+		exit(EXIT_FAILURE);
+
+	}
+
+	numstack[n_numstack++] = num;
+
+}
+
+void insert_numstack(double num, char* in)
+{
+	char* temp = malloc(sizeof(char) * 20);
+	operator* checkop = NULL;
+
+	if(n_numstack > MAXNUM - 1) {
+
+		fprintf(stderr, "ERROR: Number stack overflow\n");
+		exit(EXIT_FAILURE);
+
+	}
+
+	uint8_t i = 0, j = 0, k = 0;
+
+	for(; i < n_opstack; i++) {
+
+		if(opstack[i]->op == '(') {
+
+			j = i;
+			j++;
+
+		}
+
+	}
+	
+	for(i = j; !(checkop = getop(in[i])) && (in[i] != '\0'); i++)
+		if(isdigit(in[i]) || in[i] == '.' || in[i] == ',')
+			temp[k++] = in[i];
+
+	temp[k + 1] = '\0';
+
+	for(k = 0; k < n_numstack - 1; k++) {
+
+		if(strtodec(temp) == numstack[k]) {
+
+			free(temp);
+
+			for(i = n_numstack++; i != k; i--)
+				numstack[i] = numstack[i - 1];
+
+			numstack[k] = num;
+
+			return;
+
+		}
+
+	}
+
+	free(temp);
+	
+	for(i = n_numstack++; i != 0; i--)
+		numstack[i] = numstack[i - 1];
+
+	numstack[0] = num;
+
+}
+
+double pop_numstack()
+{
+
+	if(!n_numstack) {
+
+		fprintf(stderr, "ERROR: Number stack empty\n");
+		exit(EXIT_FAILURE);
+
+	}
+
+	--n_numstack;
+	return numstack[n_numstack];
+
+}
+
+void process_op(operator* op, char* in)
+{
+
+	operator* popped_op;
+	double num1, num2;
+
+	if(op->op == '(') {
+
+		push_opstack(op);
+		return;
+
+ 	} else if(op->op == ')') {
+
+		while(n_opstack > 0 && opstack[n_opstack - 1]->op != '(') {
+
+			popped_op = pop_opstack();
+			num1 = pop_numstack();
+
+			if(popped_op->unary) {
+
+				push_numstack(popped_op->func(num1, 0));
+
+			} else {
+
+				num2 = pop_numstack();
+				push_numstack(popped_op->func(num2, num1));
+
+			}
+
+		}
+
+		if(!(popped_op = pop_opstack()) || popped_op->op != '(') {
+			
+			fprintf(stderr, "ERROR: Unmatched Parenthesis\n");
+			exit(EXIT_FAILURE);
+
+		}
+
+		return;
+
+	}
+
+	if(op->assoc == ASSOC_R) {
+
+		while(n_opstack && op->prec < opstack[n_opstack - 1]->prec) {
+
+			popped_op = pop_opstack();
+			num1 = pop_numstack();
+
+			if(popped_op->unary) {
+
+				push_numstack(popped_op->func(num1, 0));
+
+			}	else {
+
+				num2 = pop_numstack();
+				push_numstack(popped_op->func(num2, num1));
+
+			}
+
+		}
+
+	} else {
+
+		while(n_opstack && op->prec <= opstack[n_opstack - 1]->prec) {
+
+			popped_op = pop_opstack();
+			num1 = pop_numstack();
+
+			if(popped_op->unary) {
+
+				push_numstack(popped_op->func(num1, 0));
+
+			} else {
+
+				num2 = pop_numstack();
+				push_numstack(popped_op->func(num2, num1));
+
+			}
+
+		}
+
+	}
+
+	push_opstack(op);
+	
+}
---
+++ btlib.h
@@ -0,0 +1,305 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+#ifndef BUFSIZE
+#define BUFSIZE 128
+#endif
+
+// Function declarations
+
+double muminus(double, double);
+double madd(double, double);
+double msub(double, double);
+double mmul(double, double);
+double mdiv(double, double);
+double mpow(double, double);
+double mmod(double, double);
+uint16_t strasciivalsum(char*); 					// Returns the sum of the ascii values of the characters in a string
+uint8_t chartoint(char);        					// Converts a char digit into a numeric digit.
+char* merge_args(int, char**);                      // Merge args of an arg array into one array, putting no space inbetween args.
+char* get_input(size_t);
+char* rm_space(char*);                              // Remove spaces from a string
+char* cut_str(char*, uint8_t, uint8_t); 	        // Returns the string given in the parameters as a new string but starting from index_start and ending in index_end
+_Bool ismemberof(char*, uint8_t);                  // Checks if a char is an element of a string
+_Bool isarrmemberof(char**, char*);				    // Checks if an array is an element of a 2D array
+_Bool xor(_Bool, _Bool);
+double strtodec(char*); 							// Converts a number in a string into its decimal value.
+
+// Mathematical Functions
+
+double muminus(double num1, double num2) {return -num1;}
+double madd(double num1, double num2) {return num1 + num2;}
+double msub(double num1, double num2) {return num1 - num2;}
+double mmul(double num1, double num2) {return num1 * num2;}
+
+double mdiv(double num1, double num2) {
+
+	if(!num2) {
+
+		fprintf(stderr, "ERROR: Division by zero\n");
+		return EXIT_FAILURE;
+
+	}
+
+	return num1 / num2;
+
+}
+
+double mpow(double x, double y)
+{
+
+	if(y >= 1) {
+
+		return x * mpow(x, y - 1);
+
+	} else if(y == 0) {
+
+		return 1;
+
+	} else {
+
+		return 1/mpow(x, -y);
+
+	}
+
+}
+
+double mmod(double dividend, double divisor)
+{
+
+	_Bool dividend_is_negative = dividend < 0;
+	_Bool divisor_is_negative = divisor < 0;
+
+	double long result = dividend;
+
+	if(!divisor) {
+
+		fprintf(stderr, "ERROR: Division by zero\n");
+		return EXIT_FAILURE;
+
+	}
+
+	if((!dividend_is_negative && !divisor_is_negative) || (dividend_is_negative && divisor_is_negative)) {
+
+		if(!dividend_is_negative && !divisor_is_negative)
+			while(result - divisor > 0)
+				result -= divisor;
+		else
+			while(result - divisor < 0)
+				result -= divisor;
+
+	} else if(xor(dividend_is_negative, divisor_is_negative)) {
+
+		if(dividend_is_negative)
+			do {
+				result += divisor;
+			} while(result < 0);
+		else 
+			do {
+				result += divisor;
+			} while(result > 0);
+
+	}
+
+	return result;
+
+}
+
+// Others
+
+// Returns the sum of the ascii values of the characters in a string
+uint16_t strasciivalsum(char* str)
+{
+
+	uint16_t ret = 0;
+	uint8_t i = 0;
+
+	for(; str[i] != '\0'; i++)
+		ret += str[i];
+
+	return ret;
+
+}
+
+// Converts a char digit into a numeric digit.
+uint8_t chartoint(char c) 
+{ 
+
+	return c - 48; 
+
+}
+
+// Merge args of an arg array 
+// into one array, putting no space inbetween args.
+char* merge_args(int argc, char** args)
+{
+
+	char* arr = malloc(sizeof(char) * BUFSIZE);
+	uint8_t i = 1;
+
+	while(i < argc)
+		strcat(arr, args[i++]);
+
+	arr = realloc(arr, strlen(arr) + 1);
+	return arr;
+
+}
+
+char* get_input(size_t bufsize)
+{
+
+	char* in = malloc(sizeof(char) * bufsize);
+	uint8_t c = 0, i = 0;
+
+	for(; i < bufsize - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+		in[i] = c;
+	
+	if(c == '\n')
+		in[i] = '\0';
+	
+	return in;
+
+}
+
+// Remove spaces from a string
+char* rm_space(char* in)
+{
+
+	char* out = malloc(sizeof(char) * BUFSIZE);
+	uint8_t i = 0, j = 0;
+
+	for(; in[i] != '\0'; i++) {
+
+		if(in[i] != ' ')
+			out[j++] = in[i];
+
+	}
+
+	out[j] = '\0';
+	out = realloc(out, j + 1);
+	return out;
+	
+}
+
+// Returns the string given in the parameters as a new string but
+// starting from index_start and ending in index_end
+char* cut_str(char* in, uint8_t index_start, uint8_t index_end)
+{
+
+	char* ret = malloc(sizeof(char) * (index_end - index_start + 2));
+	uint8_t i = 0, j = 0;
+
+	if(index_start == index_end) {
+
+		ret = realloc(ret, sizeof(char) * 1);
+		ret[0] = in[index_start];
+		return ret;
+
+	}
+
+	while(1) {
+
+		if(i == index_start) {
+
+			while(i <= index_end)
+				ret[j++] = in[i++];
+
+		} else {
+
+			i++;
+			continue;
+
+		}
+
+		ret[j] = '\0';
+		return ret;
+
+	}
+
+}
+
+// Checks if a char is an element of a string
+_Bool ismemberof(char* str, uint8_t c)
+{
+
+	for(uint8_t i = 0; i < strlen(str); i++) {
+	
+		if(c == str[i])
+			return true;
+
+	}
+
+	return false;
+
+}
+
+// Checks if an array is an element of a 2D array
+_Bool isarrmemberof(char** arrs, char* arr)
+{
+	uint8_t j = 0;
+
+	while(arrs[j] != '\0');
+		j++;
+
+	for(uint8_t i = 0; i < j; i++) {
+
+		if(!strcmp(arr, arrs[i]))
+			return true;
+
+	}
+
+	return false;
+
+}
+
+_Bool xor(_Bool a, _Bool b)
+{
+
+	return (a || b) && !(a && b);
+
+}
+
+// Converts a number in a string into its decimal value.
+double strtodec(char* str)
+{
+
+	double dec = 0;
+	uint8_t i = 0, j = strlen(str) - 1, k = 0;
+
+	while(str[i] != '.' && str[i] != ',' && str[i] != '\0')
+		i++;
+	
+	if(str[i] == '\0') {
+
+		for(; k < i; k++) {
+
+			dec += mpow(10, k) * (double)chartoint(str[j - k]);
+
+		}
+
+		return dec;
+
+	} else {
+
+		for(; k < i; k++) {
+
+			dec += mpow(10, k) * (double)chartoint(str[j - k - 1]);
+			
+		}
+
+	}
+
+	uint8_t dec_places = j - i;
+
+	while(dec_places-- > 1) {
+
+		dec += mpow(10, -dec_places) * (double)chartoint(str[j--]);
+
+	}
+
+	return dec;
+
+}
